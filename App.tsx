8a31f59652940dfbeb038501d0078aef412b9c7b
Thais Costa <thais1fc2002@gmail.com>
2025-06-20 16:45:39 -0300

    arrumando os films

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, TouchableOpacity, TextInput, FlatList } from 'react-native';
+import { styles } from './styles';
+
+const FILMS = [
+  { id: 1, image: require('./assets/liloStitch.png') },
+  { id: 2, image: require('./assets/Mickey17.png') },
+  { id: 3, image: require('./assets/interestellar.png') },
+  { id: 4, image: require('./assets/materialist.png') },
+  { id: 5, image: require('./assets/sinners.png') },
+  { id: 6, image: require('./assets/darkNight.png') },
+  { id: 7, image: require('./assets/homemComH.png') },
+  { id: 8, image: require('./assets/inception.png') },
+];
+
+
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <View >
+      <View style={styles.Header}>
+        <Text style={styles.title}>Popular</Text>
+        <TouchableOpacity style={styles.butonnBusca} >
+          <Text style={styles.lupa}>🔎</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View >
+        <TouchableOpacity style={styles.menus}>
+          <Text style={styles.text}>FILMS</Text>
+          <Text style={styles.text}>REVIEWS</Text>
+          <Text style={styles.text}>LISTS</Text>
+          <Text style={styles.text}>JOURNAL</Text>
+        </TouchableOpacity>
+      </View >
+
+      <FlatList
+        data={FILMS}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        contentContainerStyle={styles.grid}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Image source={item.image} style={styles.image} />
+          </View>
+        )}
+
+      />
+
+
+
+
+    </View >
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.ts
@@ -0,0 +1,60 @@
+import { StyleSheet } from "react-native";
+import { Header } from "react-native/Libraries/NewAppScreen";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1f1f1f',
+  },
+  Header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  butonnBusca: {
+    backgroundColor: '#333',
+    padding: 8,
+    borderRadius: 20,
+  },
+  lupa: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  menus: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: '#555',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  grid: {
+    padding: 8,
+  },
+  card: {
+    flex: 1,
+    margin: 4,
+    aspectRatio: 2 / 3,
+    borderRadius: 8,
+    overflow: 'hidden',
+    backgroundColor: '#333',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+
+});
+
+
+
